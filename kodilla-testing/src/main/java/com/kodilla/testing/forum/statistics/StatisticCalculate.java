f27db6c20f894f86397e197a8ac69f8da008ea4b
grzegomill <grzegomill@gmail.com>
2017-12-14 22:45:36 +0100

    Stub and Mock

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticCalculate.java
@@ -0,0 +1,65 @@
+package com.kodilla.testing.forum.statistics;
+
+public class StatisticCalculate
+
+{
+    /*
+Ilość użytkowników
+Ilość postów
+Ilość komentarzy
+Średnia ilość postów na użytkownika
+Średnia ilość komentarzy na użytkownika
+Średnia ilość komentarzy na post
+*/
+    private int usersCount;
+    private int postsCount;
+    private int commentCount;
+    private double avgPostsPerUser;
+    private double avgCommentsPerUser;
+    private double avgCommentsPerPost;
+
+    public void calculateAdvStatistics(Statistics statistics) {
+
+        postsCount = statistics.postsCount();
+        commentCount = statistics.commentsCount();
+        usersCount = statistics.usersNames().size();
+
+        avgPostsPerUser = (usersCount == 0) ? 0 : (double) postsCount / usersCount;
+        avgCommentsPerPost = (postsCount == 0) ? 0 : (double) commentCount / postsCount;
+        avgCommentsPerUser = (usersCount == 0) ? 0 : (double) commentCount / usersCount;
+
+    }
+
+    public int getUsersCount() {
+        return usersCount;
+    }
+
+    public int getPostsCount() {
+        return postsCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public double getAvgPostsPerUser() {
+        return avgPostsPerUser;
+    }
+
+    public double getAvgCommentsPerUser() {
+        return avgCommentsPerUser;
+    }
+
+    public double getAvgCommentsPerPost() {
+        return avgCommentsPerPost;
+    }
+
+    public void showStatistics() {
+        System.out.println("usersCount: " + usersCount);
+        System.out.println("postsCount: " + postsCount);
+        System.out.println("commentCount: " + commentCount);
+        System.out.println("avgPostsPerUser: " + avgPostsPerUser);
+        System.out.println("avgCommentsPerUser: " + avgCommentsPerUser);
+        System.out.println("avgCommentsPerPost: " + avgCommentsPerPost);
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticCalculateTestSuite.java
@@ -0,0 +1,77 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StatisticCalculateTestSuite {
+
+    private List<String> setTestUserNamesList(String str, int max) {
+
+        final List<String> userNamesMax = new ArrayList<>();
+        for (int i = 0; i < max; i++) {
+            userNamesMax.add(str + i);
+        }
+        return userNamesMax;
+    }
+
+    private void parametersTest(StatisticCalculate statCalc, Statistics statisticsMock, int expUsers, int expPosts, int expComments) {
+
+        // when
+        when(statisticsMock.usersNames()).thenReturn(setTestUserNamesList("Test_", expUsers));
+        when(statisticsMock.postsCount()).thenReturn(expPosts);
+        when(statisticsMock.commentsCount()).thenReturn(expComments);
+
+        statCalc.calculateAdvStatistics(statisticsMock);
+        //then
+        assertEquals(expUsers, statCalc.getUsersCount());
+        assertEquals(expPosts, statCalc.getPostsCount());
+        assertEquals(expComments, statCalc.getCommentCount());
+
+        assertEquals(expPosts == 0 ? 0 : (double) expComments / expPosts, statCalc.getAvgCommentsPerPost(), 0.0);
+        assertEquals(expUsers == 0 ? 0 : (double) expComments / expUsers, statCalc.getAvgCommentsPerUser(), 0.0);
+        assertEquals(expUsers == 0 ? 0 : (double) expPosts / expUsers, statCalc.getAvgPostsPerUser(), 0.0);
+
+        statCalc.showStatistics();
+    }
+
+
+    @Test
+    public void testCalculateAdvStatistic() {
+
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticCalculate statCalc = new StatisticCalculate();
+
+        //gdy liczba użytkowników = 0   - wszystko 0
+        statCalc.calculateAdvStatistics(statisticsMock);
+        assertEquals(0, statCalc.getUsersCount());
+
+        //gdy liczba użytkowników = 100  - reszta dowolna
+        this.parametersTest(statCalc, statisticsMock, 100, 50, 90);
+
+        //gdy liczba postów = 0 - reszta dowolna
+        this.parametersTest(statCalc, statisticsMock, 13, 0, 0);
+
+        //gdy liczba postów = 0 - reszta dowolna
+        this.parametersTest(statCalc, statisticsMock, 13, 0, 0);
+
+        //gdy liczba komentarzy = 0 - reszta dowolna
+        this.parametersTest(statCalc, statisticsMock, 37, 23, 0);
+
+        //gdy liczba postów = 1000 - reszta dowolna
+        this.parametersTest(statCalc, statisticsMock, 531, 1000, 1000);
+
+        //gdy liczba komentarzy < liczba postów
+        this.parametersTest(statCalc, statisticsMock, 422, 1000, 333);
+
+        //gdy liczba komentarzy > liczba postów
+        this.parametersTest(statCalc, statisticsMock, 7531, 321, 777);
+
+    }
+}

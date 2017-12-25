b026e485929500622aa57fbbc115c25a575551e5
grzegomill <grzegomill@gmail.com>
2017-12-25 15:18:19 +0100

    Wstęp  do programowania funkcyjnego

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,9 +1,27 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+
 public class StreamMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 -Stream");
 
+        Processor processor = new Processor();
+        // ExecutorSaySamothing executorSaySamothing = new ExecutorSaySamothing();
+
+        Executor codeToExecute = () -> System.out.println("This is lambda text.");
+        processor.execute(codeToExecute);
+
+        codeToExecute = () -> System.out.println("This is another lambda text.");
+        processor.execute(codeToExecute);
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/lambda/ExecutorSaySamothing.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/ExecutorSaySamothing.java
@@ -1,9 +1,9 @@
 package com.kodilla.stream.lambda;
 
-public class SaySomething {
-    //
+public class ExecutorSaySamothing implements Executor {
 
-    public void say() {
+    @Override
+    public void process() {
         System.out.println("This is an example text.");
     }
 }

102ad966745a070fc26095a189bd124f5a3de0e6
grzegomill <grzegomill@gmail.com>
2017-12-25 20:51:47 +0100

    Zadanie: Niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
@@ -53,6 +54,10 @@ public class StreamMain {
                 }
 
         );
+
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }

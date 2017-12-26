8b0cdf49a6e8fe7cbc77f2227ddcdd66ff0105b0
grzegomill <grzegomill@gmail.com>
2017-12-26 23:52:38 +0100

    StreamMain update

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,37 +1,10 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-/*
-
-        Processor processor = new Processor();
-        // ExecutorSaySamothing executorSaySamothing = new ExecutorSaySamothing();
-
-        Executor codeToExecute = () -> System.out.println("This is lambda text.");
-        processor.execute(codeToExecute);
-
-        codeToExecute = () -> System.out.println("This is another lambda text.");
-        processor.execute(codeToExecute);
-
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-*/
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
@@ -56,8 +29,43 @@ public class StreamMain {
         );
 
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+
+
+
+        /*
+        Processor processor = new Processor();
+        // ExecutorSaySamothing executorSaySamothing = new ExecutorSaySamothing();
+
+        Executor codeToExecute = () -> System.out.println("This is lambda text.");
+        processor.execute(codeToExecute);
+
+        codeToExecute = () -> System.out.println("This is another lambda text.");
+        processor.execute(codeToExecute);
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+*/
+//        System.out.println("Using Stream to generate even numbers from 1 to 20");
+//        NumbersGenerator.generateEven(20);
+
+
+       /* People.getList().stter(s -> s.length() > 11)
+                .forEach(System.out::println);*/
+
+
+
 
     }
 }

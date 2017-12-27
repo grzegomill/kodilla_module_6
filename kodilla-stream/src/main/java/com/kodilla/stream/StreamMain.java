da70ed6643e773d40f55dc81252c8de95c1a2cfb
grzegomill <grzegomill@gmail.com>
2017-12-27 13:43:09 +0100

    StreamMain update

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -29,43 +29,5 @@ public class StreamMain {
         );
 
 
-
-
-
-        /*
-        Processor processor = new Processor();
-        // ExecutorSaySamothing executorSaySamothing = new ExecutorSaySamothing();
-
-        Executor codeToExecute = () -> System.out.println("This is lambda text.");
-        processor.execute(codeToExecute);
-
-        codeToExecute = () -> System.out.println("This is another lambda text.");
-        processor.execute(codeToExecute);
-
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-*/
-//        System.out.println("Using Stream to generate even numbers from 1 to 20");
-//        NumbersGenerator.generateEven(20);
-
-
-       /* People.getList().stter(s -> s.length() > 11)
-                .forEach(System.out::println);*/
-
-
-
-
     }
 }

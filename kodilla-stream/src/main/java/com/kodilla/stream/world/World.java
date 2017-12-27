3de819191a316130935dd914539b8627e5b84443
grzegomill <grzegomill@gmail.com>
2017-12-27 23:50:34 +0100

    World test suite

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream.world;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,5 +30,14 @@ public final class World {
         return continents.remove(continent);
     }
 
+
+    public BigDecimal getPeopleQuantity() {
+
+        return continents.stream()
+                .flatMap(continent -> continent.getCountries().stream())
+                .map(country -> country.getPeopleQuantity())
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+
+    }
 
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -3,13 +3,41 @@ package com.kodilla.stream.world;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class WorldTestSuite {
 
     @Test
     public void testGetPeopleQuantity() {
 
-        Assert.assertTrue(false);
+        World earth = new World("Earth");
+
+        Continent asia = new Continent("Asia");
+        Continent europe = new Continent("Europe");
+        Continent america = new Continent("America");
+
+        earth.addContinent(europe);
+        earth.addContinent(america);
+        earth.addContinent(asia);
+
+        europe.addCountry(new Country("Poland", new BigDecimal("38476269")));
+        europe.addCountry(new Country("France", new BigDecimal("67106161")));
+        europe.addCountry(new Country("Germany", new BigDecimal("80594017")));
+
+        america.addCountry(new Country("USA", new BigDecimal("326625791")));
+        america.addCountry(new Country("Mexico", new BigDecimal("124574795")));
+        america.addCountry(new Country("Canada", new BigDecimal("35623680")));
+
+        asia.addCountry(new Country("China", new BigDecimal("1379302771")));
+        asia.addCountry(new Country("Japan", new BigDecimal("126451398")));
+        asia.addCountry(new Country("Russia", new BigDecimal("142257519")));
+
+        BigDecimal worldPeopleQuantity = earth.getPeopleQuantity();
+
+        BigDecimal expectedWorldPeopleQuantity = new BigDecimal("2321012401");
+
+        Assert.assertEquals(expectedWorldPeopleQuantity, worldPeopleQuantity);
+
+
     }
-
-
 }

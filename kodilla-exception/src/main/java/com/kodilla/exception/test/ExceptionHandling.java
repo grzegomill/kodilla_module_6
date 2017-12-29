6658c998b6040933fb5ee073ad6599f837f3b30e
grzegomill <grzegomill@gmail.com>
2017-12-29 23:11:04 +0100

    Zadanie: Obsługa wyjątku

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,17 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(2, 3));
+        } catch (Exception e) {
+            System.out.println("Wystąpił wyjątek: " + e);
+        }
+    }
+
+}

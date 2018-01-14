702fbb6b9ce378ccb919f7186d945d96609770cf
grzegomill <grzegomill@gmail.com>
2018-01-14 23:07:40 +0100

    Zadanie: Tablica z listami zadań

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardTestSuite {
+
+    @Test
+    public void testTaskAdd() {
+
+
+    }
+}

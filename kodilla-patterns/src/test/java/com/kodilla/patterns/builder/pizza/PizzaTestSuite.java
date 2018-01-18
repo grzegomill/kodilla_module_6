317f2835a79b2830c3b998fce566bbe9556fa179
grzegomill <grzegomill@gmail.com>
2018-01-19 00:42:50 +0100

    Builder - example pizza

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/pizza/PizzaTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/pizza/PizzaTestSuite.java
@@ -6,11 +6,21 @@ import org.junit.Test;
 public class PizzaTestSuite {
     @Test
     public void testPizzaNew() {
+
         //Given
-        Pizza pizza = new Pizza("Thin", "Spicy", "Mushrooms", "Onion", "Ham");
+        Pizza pizza = new Pizza.PizzaBuilder()
+                .ingredient("Onion")
+                .bottom("Thin")
+                .sauce("Spicy")
+                .ingredient("Ham")
+                .ingredient("Mushrooms")
+                .build();
+
         System.out.println(pizza);
+
         //When
         int howManyIngredients = pizza.getIngredients().size();
+
         //Then
         Assert.assertEquals(3, howManyIngredients);
     }

244edcfd69478a84ef19d92630fef0e41d4e60e6
grzegomill <grzegomill@gmail.com>
2018-01-21 14:36:07 +0100

    testSelectUsersAndPosts()

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -8,6 +8,42 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DbManagerTestSuite {
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "select u.FIRSTNAME, u.LASTNAME, count(1) AS POSTS " +
+                "from users u, posts p " +
+                "where p.USER_ID=u.ID " +
+                "group by p.USER_ID  " +
+                "having count(1)>1;";
+
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+
+        System.out.println("\nFIRSTNAME, LASTNAME, POSTS");
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(
+                    rs.getString("FIRSTNAME") + ", " +
+                            rs.getString("LASTNAME") + ", " +
+                            rs.getInt("POSTS"));
+            counter++;
+        }
+
+        rs.close();
+        statement.close();
+
+        Assert.assertEquals(2, counter);
+
+    }
+
     @Test
     public void testConnect() throws SQLException {
         //Given
@@ -24,6 +60,8 @@ public class DbManagerTestSuite {
 
         //When
         String sqlQuery = "SELECT * FROM USERS";
+
+
         Statement statement = dbManager.getConnection().createStatement();
         ResultSet rs = statement.executeQuery(sqlQuery);
 

647ee9290807f1c08ea9293d16e7b6c3756dcce6
grzegomill <grzegomill@gmail.com>
2018-04-23 21:19:29 +0200

    Pattern adapter

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -18,7 +18,7 @@ public class MedianAdapterTestSuite {
         //Given
         Set<BookA> bookSet = new HashSet<>();
 
-        bookSet.add(new BookA("Adam Mickiewicz", "Pan tadeusz", 1834, "11111"));
+        bookSet.add(new BookA("Adam Mickiewicz", "Pan Tadeusz", 1834, "11111"));
         bookSet.add(new BookA("Aleksander Fredro", "Zemsta", 1838, "22222"));
         bookSet.add(new BookA("Bolesław Prus", "Katarynka", 1880, "33333"));
         bookSet.add(new BookA("Bolesław Prus", "Lalka", 1890, "44444"));
